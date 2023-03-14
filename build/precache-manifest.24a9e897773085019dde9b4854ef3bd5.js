900305d8158246bfe8f0db87fff076a6a7aacc65
Tanuj <tanujrm44@gmail.com>
2023-03-14 12:20:22 +0530

    frontend uploaded

---
+++ build/precache-manifest.24a9e897773085019dde9b4854ef3bd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3c7657dee57410f9142302982b61db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6479f5b6f1a56e0a7a9",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6479f5b6f1a56e0a7a9",
+    "url": "/static/js/main.64eabc86.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
